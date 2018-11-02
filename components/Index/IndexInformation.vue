40cd6d3682b525778563d789dba61a07c92c3f5a
choiteageun <pioneer2001@naver.com>
2018-11-03 04:21:17 +0900

    asasfaqqwqqq

--- components/Index/IndexInformation.vue
+++ components/Index/IndexInformation.vue
@@ -2,6 +2,7 @@
   <div style="margin-bottom : 40px;">
     <div class="backgroundImg">
       <div style="background-color: rgba(255,255,255,0.5);"></div>
+      <div>가</div>
     </div>
   </div>
 </template>
@@ -25,5 +26,8 @@ export default {
   background-repeat: no-repeat;
   background-size: cover;
   background-position: 50% 50%;
+  box-shadow:0px 0px 5px 0px rgba(0,0,0,0.8),
+  0px 10px 10px 0px rgba(0,0,0,0.4);
+  background-attachment: fixed;
 }
 </style>
