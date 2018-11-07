5eb8fa834a4c91e963d4dd28d5516fd1bca53d9f
choiteageun <pioneer2001@naver.com>
2018-11-07 23:29:13 +0900

    qwjdhqjdh

--- pages/product.vue
+++ pages/product.vue
@@ -3,9 +3,9 @@
     <BaseSubHeader>대출 한도 조회</BaseSubHeader>
     <div style="display:flex;">
       <div class="flex leftBtns">
-        <el-button @click="tab = 'freelancer'" :type="(tab === 'freelancer') ? 'primary' : 'warning'">프리랜서 대출</el-button>
-        <el-button type="primary">직장인 대출</el-button>
-        <el-button type="primary">사업자 대출</el-button>
+        <el-button @click="tab = 'freelancer'" :type="(tab === 'freelancer') ? 'warning' : 'primary'">프리랜서 대출</el-button>
+        <el-button @click="tab = 'worker'" :type="(tab === 'worker') ? 'warning' : 'primary'">직장인 대출</el-button>
+        <el-button @click="tab = ''" type="primary">사업자 대출</el-button>
         <el-button type="primary">대학생/무직자 대출</el-button>
         <el-button type="primary">회생 대출</el-button>
         <el-button type="primary">무입고 자동차 대출</el-button>
@@ -13,21 +13,20 @@
         <el-button type="primary">군미필 대출</el-button>
         <el-button type="primary">전/월세 대출</el-button>
         <el-button type="primary">무설정 담보 대출</el-button>
-        <!-- <el-button @click="tab = 'staff'" :type="tab === 'staff' ? 'primary' : 'warning'">직원관리</el-button> -->
       </div>
       <div>
-      <FreelancerLoan v-if="tab === 'freelancer'"></FreelancerLoan>
-      <!-- <WorkerLoan v-else-if="tab === 'worker'"></WorkerLoan> -->
-      <!-- <BuisnesLoan v-else></BuisnesLoan> -->
-      <!-- </transition> -->
-    </div>
+        <FreelancerLoan v-if="tab === 'freelancer'"></FreelancerLoan>
+      </div>
     </div>
   </div>
-
+  <!-- <el-button @click="tab = 'staff'" :type="tab === 'staff' ? 'primary' : 'warning'">직원관리</el-button> -->
+  <!-- <WorkerLoan v-else-if="tab === 'worker'"></WorkerLoan> -->
+  <!-- <BuisnesLoan v-else></BuisnesLoan> -->
+  <!-- </transition> -->
 </template>
 <script>
 import BaseSubHeader from '@/components/Base/BaseSubHeader.vue'
-import FreelancerLoan from '@/components/product/Freelancer.vue'
+import FreelancerLoan from '@/components/product/ProductFreelancer.vue'
 export default {
   data() {
     return {
@@ -43,8 +42,6 @@ export default {
 <style lang="scss" scoped>
 .article {
   padding-top: 60px;
-  padding-bottom: 60px;
-  min-height: 300vh;
   .flex {
     display: flex;
     width: 200px;
