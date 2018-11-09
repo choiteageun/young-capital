8c099b4545b35894907d9b0843fa1cce13f25da3
choiteageun <pioneer2001@naver.com>
2018-11-10 06:08:17 +0900

    asdasjhqwuye

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,8 @@ module.exports = {
   ** Plugins to load before mounting the App
   */
   plugins: [
-    '@/plugins/element-ui'
+    '@/plugins/element-ui',
+    { src: "~/plugins/aos", ssr: false },
   ],
 
   /*
