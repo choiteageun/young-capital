623c705f65cd478623129f4ac64555c24bbb5211
choiteageun <pioneer2001@naver.com>
2018-11-14 13:38:05 +0900

    asfpopspopoasas

--- components/Product/ProductApply.vue
+++ components/Product/ProductApply.vue
@@ -2,7 +2,7 @@
   <div>
     <el-row>
       <el-col style="margin-top:10px;">
-        <h4 style="color:red">※중요:모든 서류에는 주민번호 뒷자리가 않보이게 첨부(예:123456-1******)</h4>
+        <h4 style="color:red;text-align:center;margin-bottom:20px;">※중요:모든 서류에는 주민번호 뒷자리가 않보이게 첨부(예:123456-1******)</h4>
       </el-col>
     </el-row>
     <div class="formContainer">
@@ -50,10 +50,114 @@
 <script>
 export default {
   data() {
-    return {}
+    return {
+      createUserData: {
+        name: '',
+        tel: '',
+        loanAmount: '',
+        route: '웹DB',
+        agree: false
+      },
+      rules: {
+        name: [
+          { required: true, message: '이름을 입력해주세요.', trigger: 'blur' }
+        ],
+        tel: [
+          { required: true, message: '연락처을 입력해주세요.', trigger: 'blur' }
+        ],
+        loanAmount: [
+          {
+            required: true,
+            message: '대출금액을 입력해주세요.',
+            trigger: 'blur'
+          }
+        ],
+        note: [
+          {
+            required: true,
+            message: '문의내용을 입력해주세요.',
+            trigger: 'blur'
+          }
+        ],
+        agree: [
+          {
+            type: 'string',
+            message: '개인정보 취급방침을 동의해주세요.',
+            trigger: 'change'
+          }
+        ]
+      }
+    }
   },
-  methods: {}
+  methods: {
+    async createUser() {
+      console.log(this.$refs)
+      console.log(this.$refs.consulForm)
+      this.$refs.consulForm.validate(async valid => {
+        if (valid) {
+          //검증성공
+          const res = await axios.post('/api/consultation', {
+            data: this.createUserData
+          })
+        } else {
+          //검증실패
+          return false
+        }
+      })
+    }
+  }
 }
 </script>
 <style lang="scss" scoped>
+.formContainer {
+  width: 100%;
+  max-width: 800px;
+  margin-left: auto;
+  margin-right: auto;
+  .el-form {
+    box-shadow: inset 0px 0px 20px 0px rgba(0, 0, 0, 0.3),
+      inset 0px 0px 3px 0px rgba(0, 0, 0, 0.6);
+    border-radius: 5px;
+    padding-top: 8px;
+    padding-left: 8px;
+    padding-right: 8px;
+    padding-bottom: 1px;
+    margin-bottom: 10px;
+    margin-right: 10px;
+    margin-left: 10px;
+  }
+
+  .terms {
+    margin-left: 15px;
+    padding: 2px 10px;
+    max-width: 100px;
+    border: 1px solid black;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.2s;
+    font-weight: 500;
+    display: inline;
+    text-align: center;
+  }
+  .terms:hover {
+    background-color: #715d60;
+    font-weight: 600;
+    color: white;
+  }
+  .el-form > .el-form-item /deep/ .el-form-item__content {
+    margin-left: 0 !important;
+  }
+  .nameInput {
+    padding-right: 10px;
+    @media (max-width: 767px) {
+      padding-right: 0px;
+    }
+  }
+  .loanInput {
+    padding-left: 10px;
+    @media (max-width: 767px) {
+      padding-left: 0px;
+    }
+  }
+}
 </style>
